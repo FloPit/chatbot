c6a5c804d9e50155f96eefec63761dbb3c05c62e
FloPit <florian.pitschi@googlemail.com>
2018-06-05 17:05:22 +0200

    hintergrundbild und namen geandertx

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -5,7 +5,7 @@ var sessionrout = require('../app');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.io.emit("socketToMe", "chatbot");
-  res.render('chatbot', { title: 'Chatbot Anna' , session1:req.sessionID});
+  res.render('chatbot', { title: 'Chatbot Florian' , session1:req.sessionID});
 //sessionrout.sendSession(req.session, req.sessionID);
 });
 
